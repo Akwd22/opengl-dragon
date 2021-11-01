a398eac0f9f3addff1bbf35665385808e0545f15
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2021-11-01 16:18:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+/*
+* TODO :
+*   - Modéliser le dragon.
+*   - Modéliser au moins 1 primitive à partir de sa représentation paramétrique.
+*   - Utiliser au moins 2 textures : une plaquée sur une face, l'autre enroulée autour d'une primitive.
+*   - Gérer au moins 2 types de lumières.
+*   - Zoomer la caméra avec les touches 'z' et 'Z'.
+*   - Touches directionnelles :
+*       * tourner autour du dragon par la droite (flèche gauche);
+*       * tourner autour du dragon par la gauche (flèche droite);
+*       * tourner autour du dragon par le bas (flèche haute);
+*       * tourner autour du dragon par le haut (flèche basse).
+*   - Au moins 1 animation manuelle avec les touches du clavier.
+*   - Au moins 1 animation automatique.
+*/
+
 int main(int argc, char** argv)
 {
     cout << "Hello world!" << endl;
