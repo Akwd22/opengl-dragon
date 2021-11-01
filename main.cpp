9d96bb883abcfa4aba0f49f535273f0b11d96ce2
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2021-11-01 17:43:06 +0100

    Initialisation GL et GLUT

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+
+#ifdef __APPLE__
+    #include <GLUT/glut.h> /* Pour Mac OS X */
+#else
+    #include <GL/glut.h>   /* Pour les autres systèmes */
+#endif
+
 #include "consts.h"
 
 using namespace std;
@@ -19,10 +26,100 @@ using namespace std;
 *   - Au moins 1 animation automatique.
 */
 
+/*
+* NOTES :
+*   - Hauteur : axe Y.
+*/
+
+void initGlut(int* argc, char** argv);
+void initGl();
+
+void displayHandler();
+void reshapeHandler(int l, int h);
+void keyboardHandler(unsigned char touche, int x, int y);
+void mouseHandler(int bouton, int etat, int x, int y);
+void motionHandler(int x, int y);
+void specialHandler(int touche, int x, int y);
+void idleHandler();
+
 #if MAIN_CUBE == 0
 int main(int argc, char** argv)
 {
-    cout << "Hello world!" << endl;
+    initGlut(&argc, argv);
     return 0;
 }
 #endif // MAIN_CUBE
+
+void initGlut(int* argc, char** argv)
+{
+    // Initialisation fenêtre.
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(2200, 200);
+    glutCreateWindow("Projet SI");
+
+    // Initialiser OpenGL.
+    initGl();
+
+    // Définition des fonctions de rappel.
+    glutDisplayFunc(displayHandler);
+    glutReshapeFunc(reshapeHandler);
+    glutIdleFunc(idleHandler);
+
+    glutKeyboardFunc(keyboardHandler);
+    glutMouseFunc(mouseHandler);
+    glutMotionFunc(motionHandler);
+    glutSpecialFunc(specialHandler);
+
+    glutMainLoop();
+}
+
+void initGl()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0); // Couleur du fond.
+    glColor3f(1.0, 1.0, 1.0); // Couleur des tracés.
+
+    glPointSize(2.0);
+
+    glEnable(GL_DEPTH_TEST);
+}
+
+void displayHandler()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSolidTeapot(0.5);
+
+    glFlush();
+    glutSwapBuffers();
+};
+
+void reshapeHandler(int l, int h)
+{
+    cout << "Window reshaped with new width: " << l << " and height: " << h << endl;
+};
+
+void keyboardHandler(unsigned char touche, int x, int y)
+{
+    cout << "Key pressed: " << touche << " at X: " << x << " Y: " << y << endl;
+};
+
+void mouseHandler(int bouton, int etat, int x, int y)
+{
+    cout << "Mouse button pressed: " << bouton << " with state: " << etat << " at X: " << x << " Y: " << y << endl;
+};
+
+void motionHandler(int x, int y)
+{
+    cout << "Mouse moved while button is pressed at X: " << x << " Y: " << y << endl;
+};
+
+void specialHandler(int touche, int x, int y)
+{
+    cout << "Non-ASCII key pressed: " << touche << " at X: " << x << " Y: " << y << endl;
+};
+
+void idleHandler()
+{
+};
